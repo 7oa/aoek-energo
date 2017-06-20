faaa8b6b70714dc0d2218b8dd22dfcedd0c6eb6b
7oa <semenova.7oa@yandex.ru>
2017-06-20 18:01:41 +0300

    new page

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -7,4 +7,19 @@ $(document).ready(function() {
 		spaceBetween: 32,
 		loop: true
 	});
+
+	if($(".about-bl4__content").length!=0){
+		$('.about-bl4__content').masonry({
+			columnWidth: 455,
+			itemSelector: '.shadow-bl',
+			gutter: 40,
+			isAnimated: true,
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false
+			}
+		});
+	}
+
 });
